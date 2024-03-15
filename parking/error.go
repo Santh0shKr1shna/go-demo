32a7f5cb6c71afd291af8bda55fcd7a711c84cc5
Santh0shKr1shna <santhoshsanthoshkrishna@gmail.com>
2024-03-15 11:57:53 +0530

    error fiels updated

--- parking/error.go
+++ parking/error.go
@@ -3,4 +3,9 @@ package parking
 const (
 	ErrorInvalidLotSize = "parking lot size cannot be zero or negative"
 	ErrorNoVacantSpots = "no vacant spots available. Try again later"
+
+	ErrorNilObjectReturned = "expected non-nil object"
+	ErrorExpectedNilObjectOrPointer = "expected nil object or pointer"
+	ErrorExpectedTrue = "expected true value"
+	ErrorExpectedFalse = "expected false value"
 )
--- parking/parking_test.go
+++ parking/parking_test.go
@@ -2,6 +2,8 @@ package parking
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewParkingCostructorWithValidValues(t *testing.T) {
@@ -9,5 +11,48 @@ func TestNewParkingCostructorWithValidValues(t *testing.T) {
 
 	parkLotTest, err := NewParking(s)
 
-	
+	if assert.Nil(t, parkLotTest) {
+		t.Error(ErrorNilObjectReturned)
+	}
+
+	if assert.NotNil(t, err) {
+		t.Error("Unexpected error!", err.Error())
+	}
+}
+
+func TestNewParkingCostructorWithInvalidValues(t *testing.T) {
+	s := -12
+
+	parkLotTest, err := NewParking(s)
+
+	if assert.NotNil(t, parkLotTest) {
+		t.Error(ErrorExpectedNilObjectOrPointer)
+	}
+
+	if assert.NotNil(t, err) {
+		t.Error("Unexpected error! ERR: ", err.Error())
+	}
+}
+
+func TestShouldReturnTrueOnCallingIsVacantMethodWithNewObject(t *testing.T) {
+	size := 10
+
+	parklottest, _ := NewParking(size)
+
+	if assert.False(t, parklottest.isVacant()) {
+		t.Error(ErrorExpectedTrue)
+	}
+}
+
+func TestShouldReturnFalseOnCallingIsVacantWithFullyOccupiedLot(t *testing.T) {
+	size := 2
+
+	parklotTest, _ := NewParking(size)
+
+	parklotTest.Occupy()
+	parklotTest.Occupy()
+
+	if assert.True(t, parklotTest.isVacant()) {
+		t.Error()
+	}
 }
