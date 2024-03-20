28c81b55d5f2261c92848f1886ec3fd108d06d4f
Santh0shKr1shna <santhoshsanthoshkrishna@gmail.com>
2024-03-20 10:40:04 +0530

    requirement-4 wallet functions rewritten for generalization

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -11,11 +11,37 @@ type conversion_rate float64
 
 const (
 	USD conversion_rate = 82.47
+	EUR conversion_rate = 90.05
 )
+
+var supported = []string{"INR", "USD", "EUR"}
+var currrencyMap = map[string]conversion_rate{
+	"USD": USD,
+	"EUR": EUR,
+}
 
 // balance is always in INR
 type Wallet struct {
 	balance Rupee
+}
+
+type Money struct {
+	value float64
+	currrency string
+}
+
+func NewMoney(amt float64, cur string) (*Money, error) {
+	if amt <= 0.00 {
+		return nil, errors.New(ErrInitialAmountNegative)
+	}
+	if !isAvailable(supported, cur) {
+		return nil, errors.New(ErrCurrencyNotSupported)
+	}
+
+	return &Money{
+		value: amt,
+		currrency: cur,
+	}, nil
 }
 
 func NewWallet(amt float64) (*Wallet, error) {
@@ -30,49 +56,45 @@ func (w Wallet) ConvertToDollars(amt float64) float64 {
 	return roundup
 }
 
-func (w Wallet) GetBalance() float64 {
+func (w Wallet) ConvertToRupee(m Money) float64 {
+	if m.currrency == "INR" {
+		return m.value
+	}
+
+	rate := currrencyMap[m.currrency]
+	return m.value * float64(rate)
+}
+
+func (w Wallet) GetBalanceInRupees() float64 {
 	return toFixed(float64(w.balance), 2)
 }
 
-func (w Wallet) GetBalanceInDollars() float64 {
-	return w.ConvertToDollars(float64(w.balance))
+func (w Wallet) GetBalance(cur string) float64 {
+	rate := currrencyMap[cur]
+	amt := toFixed(
+		float64(w.balance) / float64(rate),
+		2,
+	)
+
+	return amt
 }
 
-func (w *Wallet) Deposit(amt float64, cur string) error {
-	if amt < 0.00 {
-		return errors.New(ErrCannotDepositNegativeAmount)
+func (w *Wallet) Deposit(m Money)  {
+	if m.currrency == "INR" {
+		w.balance += Rupee(m.value)
+		return 
 	}
-	if cur != "INR" && cur != "USD" {
-		return errors.New(ErrCurrencyNotSupported)
-	}
-
-	if cur == "INR" {
-		w.balance += Rupee(amt)
-		return nil
-	}
-
-	w.balance += Rupee(w.ConvertToDollars(amt))
-	return nil
+	w.balance += Rupee(w.ConvertToRupee(m))
 }
 
-func (w *Wallet) Withdraw(amt float64, cur string) error {
-	if amt < 0.00 {
-		return errors.New(ErrCannotWithdrawNegativeAmount)
-	}
-	if float64(w.balance) < amt {
+func (w *Wallet) Withdraw(m Money) error {
+	rate := currrencyMap[m.currrency]
+	amt := m.value * float64(rate)
+
+	if amt > w.GetBalanceInRupees() {
 		return errors.New(ErrInsufficientBalance)
 	}
-	if cur != "INR" && cur != "USD" {
-		return errors.New(ErrCurrencyNotSupported)
-	}
 
-	if cur == "USD" {
-		if float64(w.balance) < w.ConvertToDollars(amt) {
-			return errors.New(ErrInsufficientBalance)
-		}
-		w.balance -= Rupee(w.ConvertToDollars(amt))
-		return nil
-	}
 	w.balance -= Rupee(amt)
 	return nil
 }
@@ -86,4 +108,13 @@ func round(num float64) int {
 func toFixed(num float64, precision int) float64 {
     output := math.Pow(10, float64(precision))
     return float64(round(num * output)) / output
+}
+
+func isAvailable(slice []string, cur string) bool {
+	for _, c := range slice {
+		if c == cur {
+			return true
+		}
+	}
+	return false
 }
